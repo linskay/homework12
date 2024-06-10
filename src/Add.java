f9c5785641b72c770b26f5cbf7413a76cd1de4cb
linskay <alina.cheremiskina@gmail.com>
2024-06-11 00:46:10 +0300

    complete

---
+++ src/Add.java
@@ -0,0 +1,14 @@
+public class Add {
+    public static void main(String[] args) {
+        Author createAuthorOne = new Author("Динозавр", "Пупыркин");
+        System.out.println(createAuthorOne.getNameAuthor() + " " + createAuthorOne.getSurnameAuthor());
+        Author createAuthorTwo = new Author("Пират", "Обормот");
+        System.out.println(createAuthorTwo.getNameAuthor() + " " + createAuthorTwo.getSurnameAuthor());
+        Book createBookOne = new Book("Как перестать страдать фигней?", createAuthorOne, 2024);
+        System.out.println(createBookOne.getNameBook() + " " + createBookOne.getWriter().getNameAuthor() + " " + createBookOne.getWriter().getSurnameAuthor() + ", " + createBookOne.getYear());
+        Book createBookTwo = new Book("Это безумие? Нет! Это JAVA!", createAuthorTwo, 2024);
+        System.out.println(createBookTwo.getNameBook() + " " + createBookTwo.getWriter().getNameAuthor() + " " + createBookTwo.getWriter().getSurnameAuthor() + ", " + createBookTwo.getYear());
+        createBookTwo.setYear(2021);
+        System.out.println(createBookTwo.getNameBook() + " " + createBookTwo.getWriter().getNameAuthor() + " " + createBookTwo.getWriter().getSurnameAuthor() + ", " + createBookTwo.getYear());
+    }
+}
---
+++ src/Author.java
@@ -0,0 +1,20 @@
+public class Author {
+    private String nameAuthor;
+    private String surnameAuthor;
+
+    public Author(String nameAuthor, String surnameAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    public String getNameAuthor() {
+        return this.nameAuthor;
+    }
+
+    public String getSurnameAuthor() {
+        return this.surnameAuthor;
+    }
+    //public String fullName (){
+    //return nameAuthor+ " "+ surnameAuthor; //- почему не работает объединение?:(
+}
+
